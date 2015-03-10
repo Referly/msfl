fdf476a28a1277709a94ebbebffdb2350f57b7ee
Courtland Caldwell <courtland@mattermark.com>
2015-03-10 09:50:30 -0700

    Added self registration of movies dataset

--- lib/msfl/datasets/movies.rb
+++ lib/msfl/datasets/movies.rb
@@ -5,6 +5,8 @@ module MSFL
     # This is a fake dataset definition that shows the structure for composing your own and is used for testing
     #  msfl
     class Movies < ::MSFL::Datasets::Base
+      register_dataset
+
       def fields
         [:title, :rating, :description, :earnings]
       end
